6094e87b5bf53c4c6c1f2e8c28868b80820d8779
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-04-02 11:36:30 +0300

    Метод замены всех вхождений слова в строке

--- FP_HW_16_2_Task_1.cpp
+++ FP_HW_16_2_Task_1.cpp
@@ -5,30 +5,30 @@
 
 
 char* ReplaceWord(char* str, const char* oldWord, const char* newWord) {
-	size_t oldWordLength = strlen(oldWord);
-	size_t newWordLength = strlen(newWord);
+    size_t oldWordLength = strlen(oldWord);
+    size_t newWordLength = strlen(newWord);
 
-	size_t newStrSize = CalcSizeNewStr(str, oldWord, newWord) + 1;
-	char* newStr = new char[newStrSize];
+    size_t newStrSize = CalcSizeNewStr(str, oldWord, newWord) + 1;
+    char* newStr = new char[newStrSize] {};
+    
+    char* ptr = str;
+    while ((ptr = strstr(ptr, oldWord)) != nullptr) {
+        // Для цикла надо каждый раз копировать с текущего конца, поэтому newStr + strlen(newStr)
+        strncpy_s(newStr + strlen(newStr), newStrSize - strlen(newStr), str, ptr - str);
+        // Добавляем новое слово
+        strcat_s(newStr, newStrSize, newWord);
+        // Перемещаем ptr на следующую позицию после найденного слова
+        ptr += oldWordLength;
+        // Перемещаем str на следующую позицию после найденного слова
+        str = ptr;
+    }
 
-	char* ptr = str;
-	while ((ptr = strstr(ptr, oldWord)) != nullptr) {
-		if (*(ptr + oldWordLength) == ' ' ||
-			*(ptr + oldWordLength) == '\0' ||
-			*(ptr + oldWordLength) == ',' ||
-			*(ptr + oldWordLength) == '.') { // Определяю завершение слова
-			size_t remainingChars = strlen(ptr + oldWordLength) + 1; // Количество символов после найденного слова
-			memmove(ptr + newWordLength, ptr + oldWordLength, remainingChars); // Перемещаем оставшуюся часть строки на новое место
-			memcpy(ptr, newWord, newWordLength); // Копируем новое слово вместо найденного
-			ptr += newWordLength; // Перемещаем указатель к концу нового слова
-		}
-		else {
-			ptr += oldWordLength; // Пропускаем найденное слово и переходим к следующему символу
-		}
-	}
+    // Добавляем оставшуюся часть строки str
+    strcat_s(newStr, newStrSize, str);
 
-	return str; // Возвращаем модифицированную строку
+    return newStr; // Возвращаем модифицированную строку
 }
+
 
 size_t CalcSizeNewStr(char* str, const char* oldWord, const char* newWord) {
 
